99a55bb04be86c3d2474fcf9a30fe6fb1631e642
Francois-hardy <55779879+Francois-hardy@users.noreply.github.com>
2022-03-01 11:38:45 +0100

    entity class

---
+++ src/main/java/com/course/bdavance/entity/Product.java
@@ -0,0 +1,18 @@
+package com.course.bdavance.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private String description;
+    private String illustration;
+    private Double price;
+
+
+}
