bd4ec2fab81ae30da7fedc04dbde01d089f70bbd
theo-dealmeida <theo.dealmeida30@gmail.com>
2022-03-01 11:25:07 +0100

    Initial commit

---
+++ src/main/java/com/course/bdavance/BdAvanceApplication.java
@@ -0,0 +1,13 @@
+package com.course.bdavance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdAvanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BdAvanceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/course/bdavance/BdAvanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.bdavance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdAvanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
